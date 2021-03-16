4cfadcad77545689d3c9b1d01f2cd2e77d50fb86
Tiago Miranda <miranda.f.tiago@gmail.com>
2021-03-16 22:10:14 +0000

    Persistance to Local Storage - removeBook()

--- appes6.js
+++ appes6.js
@@ -91,8 +91,16 @@ class Store {
     return books;
   }
 
-  static removeBook() {
+  static removeBook(isbn) {
+    const books = Store.getBooks();
 
+    books.forEach(function(book, index){
+      if(book.isbn === isbn) {
+        books.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('books', JSON.stringify(books));
   }
 }
 
@@ -142,6 +150,9 @@ document.getElementById('book-list').addEventListener('click', function(e){
 
   //Delete book
   ui.deleteBook(e.target);
+
+  //Remove from Local Storage
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 
   //Show message
   ui.showAlert('Book removed!', 'success');
